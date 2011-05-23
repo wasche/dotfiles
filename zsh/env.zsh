be2c1424cbfc9f0eb36422050ae0180009274016
Wil Asche <wasche@tripadvisor.com>
2011-05-23 11:11:34 -0400

    added a few more conventient functions/keybinds

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -39,6 +39,48 @@ mkdir -p ~/.zsh
 HISTFILE=~/.zsh/history
 SAVEHIST=20000
 
+zsh-fg()
+{
+  if (( ${#jobstates} )); then
+    zle .push-input
+    [[ -o hist_ignore_space ]] && BUFFER=' ' || BUFFER=''
+    BUFFER="${BUFFER}fg"
+    zle .accept-line
+  else
+    zle -M 'No background jobs. Doing nothing.'
+  fi
+}
+zle -N zsh-fg
+bindkey '^z' zsh-fg
+
+# run command line as user root via sudo:
+sudo-command-line()
+{
+  [[ -z $BUFFER ]] && zle up-history
+  if [[ $BUFFER != sudo\ * ]]; then
+    BUFFER="sudo $BUFFER"
+    CURSOR=$(( CURSOR+5 ))
+  fi
+}
+zle -N sudo-command-line
+bindkey '^Os' sudo-command-line
+
+# jump behind the first work on the cmdline
+# useful to add options
+jump_after_command()
+{
+  local words
+  words=(${(z)BUFFER})
+
+  if (( ${#words} <= 1 )); then
+    CURSOR=${#BUFFER}
+  else
+    CURSOR=${#${words[1]}}
+  fi
+}
+zle -N jump_after_command
+bindkey '^x1' jump_after_command
+
 autoload zkbd
 # setup key accordingly
 [[ -n "${key[Home]}" ]] && bindkey "${key[Home]}" beginning-of-line
--- zshrc
+++ zshrc
@@ -3,6 +3,7 @@ source ~/.zsh/completion.zsh
 source ~/.zsh/aliases.zsh
 source ~/.zsh/chpwd.profiles.zsh
 
+# todo check $OSTYPE == "darwin10.0"
 source ~/.zsh/macosx.zsh
 
 if [[ -f ~/.zsh/special/zshrc ]]; then
