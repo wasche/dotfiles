f0aa6a196813088138bab1c41b4d77e9a3c99fc4
Wil Asche <wil@asche.us>
2012-11-08 12:52:34 -0500

    source todo cfg

--- bash/todo.bash
+++ bash/todo.bash
@@ -1,6 +1,6 @@
+. ~/.todo.cfg
 alias t='todo.sh'
 . ~/.bash/todo_completion.bash
 complete -F _todo t
 
 export TODOTXT_DEFAULT_ACTION=ls
-export TODOTXT_SORT_COMMAND='env LC_COLLATE=c sort -k 2,2 -k 1,1n'
--- zsh/todo.zsh
+++ zsh/todo.zsh
@@ -1,7 +1,7 @@
+. ~/.todo.cfg
 . ~/.zsh/todo_completion.zsh
 
 export TODOTXT_DEFAULT_ACTION=ls
-export TODOTXT_SORT_COMMAND='env LC_COLLATE=c sort -k 2,2 -k 1,1n'
 
 function t() {
   if [ $# -eq 0 ]; then
