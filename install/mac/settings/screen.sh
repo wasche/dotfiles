b18ad19648c05fe3a7b72b2219399b36221acaf6
Wil Asche <wil@asche.us>
2015-11-18 12:33:17 -0500

    cleanup

--- install/mac/settings/screen.sh
+++ install/mac/settings/screen.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-###############################################################################
-# Screen                                                                      #
-###############################################################################
-
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
