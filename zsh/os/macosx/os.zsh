9fd7ba8c8d4f9fef73a731ef983edbc542121dea
Wil Asche <wil@asche.us>
2017-08-03 07:14:49 -0400

    tame homebrew

--- zsh/os/macosx/os.zsh
+++ zsh/os/macosx/os.zsh
@@ -43,3 +43,10 @@ mdb()
 }
 
 export NODE_PATH=/usr/local/share/npm/lib/node_modules
+
+
+# Cancel those terrible ideas Homebrew had of forcing update every 300 s and
+# forcing a 3 s curl on every single run.
+HOMEBREW_NO_AUTO_UPDATE=1
+HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE HOMEBREW_NO_ANALYTICS
